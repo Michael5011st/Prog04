15889441bd6e8d4cb8deea6e505531b0458ddf5f
U-Andrew-VAIO\Andrew <reedandrew19@gmail.com>
2016-03-23 15:46:39 -0400

    Commit 3.1: changed variable names.

--- Prog04.sql
+++ Prog04.sql
@@ -14,7 +14,7 @@ Create table Users(id int Unique, name VARCHAR(20));
 #creates the Rooms table;
 Create table Rooms(build VARCHAR(20), number int, Primary Key(build, number));
 #create the Reservations table
-Create table Reservations(seq int AUTO_Increment unique, date date, beginTime time, endTime time, Users int references Users(id),
+Create table Reservations(seq int AUTO_Increment unique, date date, begin time, end time, Users int references Users(id),
 Build VARCHAR(20) references Room(build), Room int references Room(number));
 
 #Inserts into Users
@@ -33,7 +33,7 @@ Insert Into Reservations values(null, '2016-03-20', '08:00', '09:00', 	1, 'PPHAC
 Create View ReservationsView
 	as Select seq as number,
 	Concat(reservations.build, '-', number) as room,
-	date, concat(beginTime, '-', endTime) as time,
+	date, concat(begin, '-', end) as time,
 	concat(id, '-', name) as user
 	from  Users inner join reservations on
 	users.id = reservations.users inner join
