21c8417b5362c4c7ece666fd2822d2b94704daa6
U-Andrew-VAIO\Andrew <reedandrew19@gmail.com>
2016-03-23 15:40:38 -0400

    Commit 3: fixed bugs, part one of project is completed.

--- Prog04.sql
+++ Prog04.sql
@@ -2,33 +2,40 @@
 Create database prg04;
 # use the database
 Use prg04;
+
+#Drop tables
+Drop Table Users;
+Drop Table Rooms;
+Drop Table Reservations;
+Drop View ReservationsView;
+
 #Create the Users
 Create table Users(id int Unique, name VARCHAR(20));
 #creates the Rooms table;
-Create table Rooms(build VARCHAR(20) unique, number int Unique);
+Create table Rooms(build VARCHAR(20), number int, Primary Key(build, number));
 #create the Reservations table
-Create table Reservations(seq int AUTO_Increment, date, begin time, end time,Users int foreign key, 
-Build VARCHAR(20) foreign key, Room int Foreign key);
+Create table Reservations(seq int AUTO_Increment unique, date date, beginTime time, endTime time, Users int references Users(id),
+Build VARCHAR(20) references Room(build), Room int references Room(number));
 
 #Inserts into Users
-Insert Into Users values(1,John);
-Insert Into Users values(2,Mary);
+Insert Into Users values(1, 'John');
+Insert Into Users values(2, 'Mary');
 
 #Inserts into Rooms
-Insert Into Rooms values(CHS,110);
-Insert Into Rooms values(PPHAC,113);
-Insert Into Rooms values(PPHAC,114);
+Insert Into Rooms values('CHS', 110);
+Insert Into Rooms values('PPHAC', 113);
+Insert Into Rooms values('PPHAC', 114);
 
 #Inserts into Reservations
-Insert Into Reservations values('2016-03-20', '08:00', '09:00', 	1, 'PPHAC', 113), 
-	('2016-04-01', '08:00', '10:00', 2, 'PPHAC', 114);
+Insert Into Reservations values(null, '2016-03-20', '08:00', '09:00', 	1, 'PPHAC', 113), 
+	(null, '2016-04-01', '08:00', '10:00', 2, 'PPHAC', 114);
 
 Create View ReservationsView
 	as Select seq as number,
-	Concat(build, '-', number) as room,
-	date, concat(begin, '-', end) as time,
-	concat(id, '-', name)
+	Concat(reservations.build, '-', number) as room,
+	date, concat(beginTime, '-', endTime) as time,
+	concat(id, '-', name) as user
 	from  Users inner join reservations on
 	users.id = reservations.users inner join
-	room on reservations.room = rooms.number AND
+	rooms on reservations.room = rooms.number AND
 	reservations.build = rooms.build;
